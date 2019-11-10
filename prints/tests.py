c92d8c5a21b8bde8852a22a29dfff63b55161359
Eric Garnick <eric.b.garnick@gmail.com>
2019-11-09 17:02:09 -0800

    Add size_abbr for PrintSizeInfo

--- prints/tests.py
+++ prints/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from prints.models import PrintSizeInfo, PrintSize
+
+
+class TestPrintsModels(TestCase):
+    def test_print_size_abbr_small(self):
+        size = PrintSize.SMALL
+        psi = PrintSizeInfo(size=size)
+        self.assertEqual(psi.size_abbr, size.value[0])
