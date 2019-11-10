908b8f27485447b3cd45eeaf3084ee10bcab276f
Eric Garnick <eric.b.garnick@gmail.com>
2019-11-09 16:24:27 -0800

    Create prints app, move Print, PrintSizeInfo to prints app

--- hlprints/settings.py
+++ hlprints/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'geospatial',
     'customers',
     'orders',
+    'prints',
 ]
 
 MIDDLEWARE = [
--- orders/models.py
+++ orders/models.py
@@ -2,29 +2,6 @@ from django.db import models
 from enumfields import Enum, EnumField
 
 
-class PrintSize(Enum):
-    SMALL = "SMALL"
-    MEDIUM = "MEDIUM"
-    LARGE = "LARGE"
-
-
-class PrintSizeInfo(models.Model):
-    """Info about a size of prints"""
-    # If no base price, the print should not be available for sale
-    base_price_cents = models.PositiveIntegerField(null=True, blank=True)
-    # If no ship price, the print should not be available for sale
-    ship_price_cents = models.PositiveIntegerField(null=True, blank=True)
-    size = EnumField(PrintSize, max_length=16)
-
-
-class Print(models.Model):
-    """A print of a Photo that has been purchased"""
-    # size_info and photo should always exist if a print exists
-    size_info = models.ForeignKey(PrintSizeInfo, on_delete=models.PROTECT)
-    photo = models.ForeignKey('catalogs.Photo', on_delete=models.PROTECT)
-    print_number = models.PositiveIntegerField()
-
-
 class OrderStatus(Enum):
     IN_CART = "IN_CART"
     SUBMITTED = "SUBMITTED"  # customer has paid already
@@ -34,7 +11,7 @@ class OrderStatus(Enum):
 
 
 class Order(models.Model):
-    print = models.ForeignKey(Print, on_delete=models.PROTECT)
+    print = models.ForeignKey('prints.Print', on_delete=models.PROTECT)
     # SET_NULL because we may need to delete client data for
     # privacy/data control compliance but still want the order record
     customer = models.ForeignKey('customers.Customer', null=True, blank=True,
