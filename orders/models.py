0f0773273f157639f6314ab2f4fb5f8d18b7b944
Eric Garnick <eric.b.garnick@gmail.com>
2019-12-15 12:01:04 -0800

    Add __str__ and __repr__ for Order and Payment models

--- orders/models.py
+++ orders/models.py
@@ -10,6 +10,7 @@ class OrderStatus(Enum):
     CANCELLED = "CANCELLED"  # order was submitted and then cancelled
 
 
+# TODO: Include ordered prints
 class Order(models.Model):
     # SET_NULL because we may need to delete client data for
     # privacy/data control compliance but still want the order record
@@ -19,6 +20,11 @@ class Order(models.Model):
                                          on_delete=models.PROTECT)
     payment = models.OneToOneField('orders.Payment', on_delete=models.PROTECT)
     order_status = EnumField(OrderStatus, max_length=16)
+
+    def __str__(self) -> str:
+        return f"Order #{self.id}: {self.customer} - {self.order_status}"
+
+    __repr__ = __str__
 
 
 class PaymentMethod(Enum):
@@ -33,6 +39,7 @@ class CreditNetwork(Enum):
     AMEX = "AMEX"
 
 
+# TODO: Include amount paid
 class Payment(models.Model):
     method = EnumField(PaymentMethod, max_length=8)
     credit_network = EnumField(CreditNetwork, max_length=16, null=True)
@@ -43,3 +50,10 @@ class Payment(models.Model):
     billing_last_name = models.CharField(max_length=64)
     billing_address = models.ForeignKey('geospatial.Address',
                                         on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        acct_num_anon = ('x' * (len(self.account_number) - 4)
+                         + self.account_number[-4:])
+        return f"{self.credit_network} ({self.method}): {acct_num_anon}"
+
+    __repr__ = __str__
