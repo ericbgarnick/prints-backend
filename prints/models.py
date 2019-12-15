43f1b04be05ca679e6951c1a68289d614bbe94fd
Eric Garnick <eric.b.garnick@gmail.com>
2019-12-15 11:46:22 -0800

    Add __str__ and __repr__ for PrintSizeInfo and Print models

--- prints/models.py
+++ prints/models.py
@@ -1,3 +1,5 @@
+import locale
+
 from django.db import models
 from enumfields import Enum, EnumField
 
@@ -16,6 +18,15 @@ class PrintSizeInfo(models.Model):
     ship_price_cents = models.PositiveIntegerField(null=True, blank=True)
     size = EnumField(PrintSize, max_length=16)
 
+    def __str__(self) -> str:
+        base_price_dollars = self.base_price_cents / 100
+        ship_price_dollars = self.ship_price_cents / 100
+        return (f"{self.size} @ "
+                f"${base_price_dollars:.2f} base + "
+                f"${ship_price_dollars:.2f} shipping")
+
+    __repr__ = __str__
+
 
 class Print(models.Model):
     """A purchased print of a Photo"""
@@ -24,3 +35,11 @@ class Print(models.Model):
     size_info = models.ForeignKey(PrintSizeInfo, on_delete=models.PROTECT)
     photo = models.ForeignKey('photos.Photo', on_delete=models.PROTECT)
     print_number = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return (f"Order num {self.order.id}: "
+                f"\"{self.photo.title}\" "
+                f"({self.print_number}/{self.photo.max_prints})"
+                f" - size: {self.size_info.size.value.lower()} ")
+
+    __repr__ = __str__
